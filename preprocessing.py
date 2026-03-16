63cb87a3e130120bac5a3c628abf3d52899facb7
Ashlyn Chor <ahc25@ic.ac.uk>
2026-03-16 21:20:59 +0000

    Implemented preprocessing functions

---
+++ preprocessing.py
@@ -0,0 +1,44 @@
+import re
+
+WINDOW_SIZE: int = 4
+vocabulary: list[str] = [] 
+word_to_index: dict[str, int] = {}
+index_to_word: dict[int, str] = {}
+
+def tokenise_corpus(corpus: list[str]) -> list[str]:
+    tokens: list[str] = []
+    for line in corpus:
+        line = line.lower()
+        line = re.sub(r'[^\w\s]', '', line)
+        words = line.split()
+
+        for word in words:
+            tokens.append(word)
+
+    return tokens
+
+def get_training_pairs(tokens: list[str]) -> list[tuple[int, int]]:
+    training_pairs: list[tuple[int, int]] = []
+    for pos in range(len(tokens)):
+        center_index = word_to_index[tokens[pos]]
+        for i in range(1, WINDOW_SIZE // 2 + 1):
+            if pos - i >= 0:
+                context_index: int = word_to_index[tokens[pos - i]]
+                training_pairs.append((center_index, context_index))
+
+            if pos + i < len(tokens):
+                context_index: int = word_to_index[tokens[pos + i]]
+                training_pairs.append((center_index, context_index))
+    return training_pairs
+
+def main():
+    with open("corpus.txt", "r") as f:
+        corpus: list[str] = f.readlines()
+
+    tokens: list[str] = tokenise_corpus(corpus)
+
+    for token in tokens:
+        if token not in word_to_index:
+            word_to_index[token] = len(word_to_index)
+            index_to_word[len(index_to_word)] = token
+            vocabulary.append(token)
